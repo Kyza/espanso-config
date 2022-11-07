56409c24b2e816c60d61b3b84292afb39b4c43c5
Kyza <kyza@kyza.net>
2022-11-07 11:05:51 -0500

    Add dice notation & crypto

--- scripts/expr.ts
+++ scripts/expr.ts
@@ -1,14 +1,42 @@
 import bigDecimal from "js-big-decimal";
+import crypto from "node:crypto";
 
-const expr = process.argv[process.argv.findIndex((arg) => arg === "--") + 1];
+const diceNotation = /(?<amount>\d+)?d(?<sides>\d+|%)/gi;
+
+function roll(amount: number, sides: number | "%"): number {
+	if (sides === 1) return amount;
+	if (sides === "%") sides = 100;
+
+	let result = 0;
+
+	while (amount > 0) {
+		result += crypto.randomInt(1, sides);
+		amount--;
+	}
+
+	return result;
+}
+
+let expr = process.argv[process.argv.findIndex((arg) => arg === "--") + 1];
 const AsyncFunction = async function () {}.constructor;
 
 try {
+	// Replace dice notation.
+	let matches;
+	while ((matches = diceNotation.exec(expr))) {
+		const rolled = roll(
+			matches.groups.amount ?? 1,
+			matches.groups.sides === "%" ? "%" : parseInt(matches.groups.sides)
+		);
+		expr = expr.replace(matches[0], rolled.toString());
+	}
+
 	const result = await AsyncFunction(
 		"bd",
 		"m",
+		"crypto",
 		`return ${expr}`
-	)(bigDecimal, Math);
+	)(bigDecimal, Math, crypto);
 	switch (typeof result) {
 		case "bigint":
 			// Remove the n at the end.
