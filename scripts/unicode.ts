1252348cd64a0ae048d67c12d85847a6c08975af
Kyza <kyza@kyza.net>
2022-11-16 16:10:11 -0500

    Ability to convert unicode into its ID

--- scripts/unicode.ts
+++ scripts/unicode.ts
@@ -1,7 +1,11 @@
 import { parse } from "std/flags/mod.ts";
 
-const args: { id: string } = parse(Deno.args, {
-	string: ["id"],
+const args: { input: string; action: string } = parse(Deno.args, {
+	string: ["input", "action"],
 });
 
-console.log(String.fromCharCode(Number(`0x${args.id}`)));
+if (args.action === "To Unicode") {
+	console.log(String.fromCharCode(Number(`0x${args.input}`)));
+} else {
+	console.log(args.input.charCodeAt(0).toString(16));
+}
